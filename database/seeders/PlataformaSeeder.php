11df7770c3a939b0679619d67c70db4876017926
LucasBelhassof <lucasbelhassof@gmail.com>
2025-12-20 22:55:45 -0300

    feat(migrations): create calendar_settings table and update PlataformaSeeder for data seeding

--- database/seeders/PlataformaSeeder.php
+++ database/seeders/PlataformaSeeder.php
@@ -2,19 +2,35 @@
 
 namespace Database\Seeders;
 
-
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
 class PlataformaSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     */
     public function run(): void
     {
-         DB::connection('content')->table('plataforma_pedido')->insert([
+        // Desabilita FK temporariamente
+        DB::connection('content')->statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        // Trunca a tabela (opcional)
+        DB::connection('content')->table('plataforma_pedido')->truncate();
+
+        // Reabilita FK
+        DB::connection('content')->statement('SET FOREIGN_KEY_CHECKS=1;');
+
+        $items = [
             ['nome' => 'Shopee'],
             ['nome' => 'Mercado Livre'],
             ['nome' => 'Amazon'],
             ['nome' => 'Loja Integrada'],
-        ]);
+        ];
+
+        // evita exception de unique quando já existe
+        DB::connection('content')->table('plataforma_pedido')->insertOrIgnore($items);
+
+        $this->command->info('Plataformas seedadas com sucesso.');
     }
 }
